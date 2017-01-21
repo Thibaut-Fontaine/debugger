d1b25f180ceb42c7728b0fe407d811a1e3119c73
Thibaut FONTAINE <tfontain@e3r13p8.42.fr>
2017-01-21 18:22:30 +0100

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/21 17:30:28 by tfontain          #+#    #+#             */
-/*   Updated: 2017/01/21 17:46:17 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/01/21 18:22:17 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,19 +62,21 @@ int			main(int argc, char **argv)
 
 	name = malloc(strlen(argv[1]) + strlen("_debug.c"));
 	buff = malloc(1);
-	strcpy(&(name[strlen(argv[1])]), "_debug.c");
+	strncpy(name, argv[1], strlen(argv[1]) - 2 * (argv[1][strlen(argv[1] - 2)] == '.'));
+	name[strlen(argv[1]) - 2] = 0;
+	strcpy(&(name[strlen(name)]), "_debug.c");
 	k = 0;
 	fd_r = open(argv[1], O_RDONLY);
-	fd_c = open(name, O_CREAT || O_WRONLY);
-	/*puthead(fd_c);
-	while (read(fd_r, &(buff[k++]), 1))
+	fd_c = open(name, O_CREAT | O_WRONLY | S_IRWXO);
+	puthead(fd_c);
+	/*while (read(fd_r, &(buff[k++]), 1))
 	{
 		if (buff[k] == '\n')
 			put_line_counter(fd_c);
 		write(fd_c, &(buff[k]), 1);
 		realloc_str(buff, k);
-	}
-	*/close(fd_r);
+	}*/
+	close(fd_r);
 	close(fd_c);
 	return (0);
 }
