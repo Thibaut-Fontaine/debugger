a92782eeb79c345aa2974930e190cd01a5e914ca
Thibaut FONTAINE <tfontain@e2r11p22.42.fr>
2017-01-22 16:28:41 +0100

    final

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/21 17:30:28 by tfontain          #+#    #+#             */
-/*   Updated: 2017/01/22 15:40:14 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/01/22 16:26:40 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-#define LINE_COUNTER "\nputnbr(cccounter__++);"
-#define LINEHEAD "\n#include <stdlib.h>\nvoid putnbr(int n){char array[11];int k;if (n == -2147483648)ft_putstr(\"-2147483648\");else if (n == 0)ft_putchar('0');else{if (n < 0){n = -n;ft_putchar('-');}k = -1;while (n){array[++k] = n % 10 + '0';n = n / 10;}while (k >= 0)ft_putchar(array[k--]);}}\nint cccounter__ = 1;\n"
+#define LINE_COUNTER "\nprintf(\"%d\\n\", "
+#define LINEHEAD "\n#include <stdio.h>\nint cccounter__ = 1;\n"
 
 static char		*realloc_str(char *str, size_t size)
 {
@@ -35,9 +36,17 @@ static char		*realloc_str(char *str, size_t size)
 	return (r);
 }
 
-void		write_in(const int fd, char *s)
+void		write_in(const int fd, char *s, size_t k)
 {
-	write(1, s, strlen(s));
+	char *tmp = s;
+	char *add;
+
+	if (k > 0)
+	{
+		s = malloc(strlen(tmp) + 10);
+		sprintf(s, "%s%d%s", tmp, (int)k, ");");
+	}
+	write(fd, s, strlen(s));
 }
 
 int			main(int argc, char **argv)
@@ -52,25 +61,33 @@ int			main(int argc, char **argv)
 	int		fd_r;
 	int		fd_c;
 	size_t	k;
-	char	*buff;
+	char	buff[1];
 	char	*name;
 
 	name = malloc(strlen(argv[1]) + strlen("_debug.c"));
-	buff = malloc(1);
 	strncpy(name, argv[1], strlen(argv[1]) - 2);
 	name[strlen(argv[1]) - 2] = 0;
 	strcpy(&(name[strlen(name)]), "_debug.c");
 	k = 0;
 	fd_r = open(argv[1], O_RDONLY);
-	fd_c = open(name, O_CREAT | O_WRONLY | S_IRWXO);
-	write_in(fd_c, LINEHEAD);
-	/*while (read(fd_r, &(buff[k++]), 1))
+	fd_c = open(name, O_WRONLY | O_CREAT, 0666);
+	write_in(fd_c, LINEHEAD, 0);
+
+	int		crochet;
+
+	crochet = 0;
+	while (read(fd_r, buff, 1))
 	{
-		if (buff[k] == '\n')
-			put_line_counter(fd_c);
-		write(fd_c, &(buff[k]), 1);
-		realloc_str(buff, k);
-	}*/
+		if (*buff == '{')
+			++crochet;
+		if (*buff == '}')
+			--crochet;
+		if (*buff == '\n')
+			++k;
+		if (*buff == '\n' && crochet > 0)
+			write_in(fd_c, LINE_COUNTER, k);
+		write(fd_c, buff, 1);
+	}
 	close(fd_r);
 	close(fd_c);
 	return (0);
