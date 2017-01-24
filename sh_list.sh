10fa941e6347f1708f183283b5935f5915afb53e
Thibaut FONTAINE <tfontain@e1r6p3.42.fr>
2017-01-24 02:59:11 +0100

    sh

---
+++ sh_list.sh
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    sh_list.sh                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tfontain <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/01/24 02:57:54 by tfontain          #+#    #+#              #
+#    Updated: 2017/01/24 02:59:03 by tfontain         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# liste de bash interessants
+
+
+# a la racine d'un projet git, permet de connaitre l'adresse du repo
+git config --get remote.origin.url
